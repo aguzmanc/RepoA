dd98267f4adc9b3652cf4e1c1c8d9080ad0919a2
IronWaco <wmagarioscalla@gmail.com>
2016-09-19 21:03:27 -0400

    MOVIMIENTO AL FIN

---
+++ SRC/Assets/DetectarTouch.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectarTouch : MonoBehaviour {
+
+	public RectTransform Cursor;
+	public RectTransform Gira;
+
+	public float Dot;
+
+
+	void Start () {
+	
+	}
+
+
+	
+	void Update () 
+	{
+		
+/*		if (Input.touchCount > 0) {
+			Debug.Log (Input.touches [0].position);
+			Gira.Rotate (0, 0, 1);
+			Cursor.position = Input.touches [0].position;
+		}
+*/
+		if (Input.touchCount >= 2) {
+			Touch a = Input.GetTouch (0);
+			Touch b = Input.GetTouch (1);
+			Vector2 prevA = (a.position - a.deltaPosition);
+			Vector2 prevB = (b.position - b.deltaPosition);
+			float delta = (prevA - prevB).magnitude - (a.position - b.position).magnitude;
+			if(Mathf.Abs(delta)>5)
+				Camera.main.fieldOfView = Mathf.Min(Mathf.Max(Camera.main.fieldOfView + delta,30),120);
+
+			Vector2 DeltaA = (a.position - prevA);
+			Vector2 DeltaB = (b.position - prevB);
+
+			Dot = Vector2.Dot (DeltaA, DeltaB);
+			if(Dot>20){
+				Vector2 mov = (DeltaA + DeltaB) / 2;
+				Camera.main.transform.Translate (mov.x, 0, 0);
+			}
+
+		}
+	}
+
+
+}
---
+++ SRC/Assets/Movimiento.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+	
+	public float SlowAction=10;
+
+
+	bool _arriba;
+	public bool Arriba {
+		get{ return _arriba;}
+		set{ _arriba = value;}
+	}
+
+	bool _abajo;
+	public bool Abajo {
+		get{ return _abajo;}
+		set{ _abajo = value;}
+	}
+
+	bool _derecha;
+	public bool Derecha {
+		get{ return _derecha;}
+		set{ _derecha = value;}
+	}
+
+	bool _izquierda;
+	public bool Izquierda {
+		get{ return _izquierda;}
+		set{ _izquierda = value;}
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		//SlowAction = SlowAction;
+		if (Arriba)
+			transform.Translate (transform.forward * SlowAction);
+
+
+		if (Abajo)
+			transform.position = transform.position + transform.forward * -SlowAction;
+		if (Izquierda)
+			transform.Rotate(0,-SlowAction,0);
+		if (Derecha)
+			transform.Rotate(0,SlowAction,0);
+	}
+
+
+
+}
