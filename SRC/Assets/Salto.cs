94877fd15f9ed5511ce009dac6694df055b19220
IronWaco <wmagarioscalla@gmail.com>
2016-09-25 16:42:43 -0400

    Intantando Aplicar el salto
    no funciona con el collider

---
+++ SRC/Assets/Salto.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Salto : MonoBehaviour {
+    Rigidbody _rb;
+
+    public float Fuerza = 100;
+
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(0, Fuerza, 0);
+            
+        }
+	
+	}
+
+    void OntriggerStay(Collider C)
+    {
+        
+    }
+}
