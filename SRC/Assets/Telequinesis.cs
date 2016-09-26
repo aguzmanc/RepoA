89a0ee3fb93eeb86e8979c928371d7b02685490d
IronWaco <wmagarioscalla@gmail.com>
2016-09-25 22:41:45 -0400

    Intentando aplicar Telequinesis
    lograda al 50%

---
+++ SRC/Assets/Telequinesis.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Telequinesis : MonoBehaviour {
+    public float Suavidad = 0.05f;
+    public GameObject Boton;
+
+    bool _posible;
+    public bool Posible
+    {
+        get { return _posible; }
+        set { _posible = value; }
+    }
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+            
+    }
+
+
+    void OnTriggerEnter(Collider C)
+    {
+        if(C.tag=="Alzable")
+        {
+            Instantiate(Boton);
+            
+        }
+    }
+    void OnTriggerStay(Collider C)
+    {
+        if(Posible)
+         C.transform.position = Vector3.Lerp(C.transform.position, transform.position, Suavidad);
+    }
+    void OnTriggerExit(Collider C)
+    {
+        if (C.tag == "Alzable")
+            Destroy(Boton);
+    }
+
+
+}
