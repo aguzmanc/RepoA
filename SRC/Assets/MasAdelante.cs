67b14dee26eac1e47bbf63f30d5be5b971ce072f
IronWaco <wmagarioscalla@gmail.com>
2016-09-30 20:57:20 -0400

    bleh

--- SRC/Assets/MasAdelante.cs
+++ SRC/Assets/MasAdelante.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MasAdelante : Personajes {
 
     public float velocidad = 10f;
-    float tiempo=5f;
+    public float tiempo=5f;
 
 
     public MasAdelante(bool Lento) : base(Lento)
